e355a3f20b1f40d4a835cffd8ea7ffe1c565c44c
Jayden H Nguyen <nguyenhuuduchuy2712@gmail.com>
2026-06-23 16:12:27 -0400

    :sparkles: new feature for v0.3

--- apps/python/app.py
+++ apps/python/app.py
@@ -4,8 +4,8 @@
 # (replace <YOUR_STUDENT_ID> with your own Seneca student ID, e.g. 10112233)
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-VERSION = "0.2"
-MESSAGE = "Hello world from the CLO835 class!"
+VERSION = "0.3"
+MESSAGE = "Hello world from the CLO835 class and 125109249!"
 PORT = 8080
 
 
